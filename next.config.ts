3a7cc00ae776b083e672bda02de276e22da9488c
rogerdarwinn-byte <rogerdarwinn-byte@users.noreply.github.com>
2026-02-02 03:26:09 +0000

    Upload codebase file next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
       "localhost:3000"
     ],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
