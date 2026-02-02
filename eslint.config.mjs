463a9748eca766eeae263fda0fb4f1d25c368586
rogerdarwinn-byte <rogerdarwinn-byte@users.noreply.github.com>
2026-02-02 02:32:25 +0000

    Upload codebase file eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,16 @@
-import { FlatCompat } from '@eslint/eslintrc'
- 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-})
- 
 const eslintConfig = [
-  ...compat.config({
-    extends: ['next/core-web-vitals'],
-  }),
+  {
+    ignores: ["**/.next/**", "**/node_modules/**"],
+  },
   {
     rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-img-element': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      'react-hooks/exhaustive-deps': 'off',
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/exhaustive-deps": "off",
     },
   },
-]
- 
-export default eslintConfig
+];
+
+export default eslintConfig;
