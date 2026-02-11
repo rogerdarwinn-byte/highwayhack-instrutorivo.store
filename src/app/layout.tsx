39660a1605f67f6efb307b0cdb3ff1e5c28915e2
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2026-02-11 15:36:21 -0300

    Atualizar GTM para dominio customizado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,20 +35,11 @@ export default function RootLayout({
         {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KVQHD2SN');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://gtm.highwayhack-instrutorivo.store/7czsumvizcb.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','8=CwdHLiIgWCMkO0w%2BOzUmVRReSEdHSxEdXwsTDh4aFAoKAhkCWwQbABYRDx0ZHxwFDU0JHRkfEA%3D%3D');`,
           }}
         />
       </head>
       <body className="antialiased">
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KVQHD2SN"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <Script
           id="orchids-browser-logs"
           src="https://slelguoygbfzlpylpxfs.supabase.co/storage/v1/object/public/scripts/orchids-browser-logs.js"
